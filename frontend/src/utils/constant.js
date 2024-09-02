36840afdd34a52bc7a0cb809ef2553feb1c97133
pranavagarwal183 <agarwalpranav177@gmail.com>
2024-09-02 18:55:40 +0530

    ..

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="jobportal-api-tau.vercel.app";
-export const JOB_API_END_POINT="jobportal-api-tau.vercel.app";
-export const APPLICATION_API_END_POINT="jobportal-api-tau.vercel.app";
-export const COMPANY_API_END_POINT="jobportal-api-tau.vercel.app";
+export const USER_API_END_POINT="https://jobportal-api-tau.vercel.app/user";
+export const JOB_API_END_POINT="https://jobportal-api-tau.vercel.app/job";
+export const APPLICATION_API_END_POINT="https://jobportal-api-tau.vercel.app/application";
+export const COMPANY_API_END_POINT="https://jobportal-api-tau.vercel.app/company";
